01f0de7f08d44ac67510b0b40bbc2d455caa7fb8
hung721999 <nguyennhuhung72@gmail.com>
2024-11-11 23:08:21 +0700

    finish less 2 3 4

--- v2.x.x/lesson4/fund_less4_exer2/src/main.c
+++ v2.x.x/lesson4/fund_less4_exer2/src/main.c
@@ -10,9 +10,10 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 /* STEP 4 - Include the header file of the logger module */
+#include <zephyr/logging/log.h>
 
 #define MAX_NUMBER_FACT 10
-#define SLEEP_TIME_MS	10 * 60 * 1000
+#define SLEEP_TIME_MS 10 * 60 * 1000
 
 #define SW0_NODE DT_ALIAS(sw0)
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
@@ -21,26 +22,30 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 /* STEP 5 - Register your code with the logger */
+LOG_MODULE_REGISTER(Less4_Exer2, LOG_LEVEL_DBG);
 
 /* STEP 7 - Replace the callback function button_pressed() */
-void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+					uint32_t pins)
 {
 	int i;
 	int j;
 	long int factorial;
-	printk("Calculating the factorials of numbers from 1 to %d:\n", MAX_NUMBER_FACT);
-	for (i = 1; i <= MAX_NUMBER_FACT; i++) {
+	LOG_INF("Calculating the factorials of numbers 1 to %d:", MAX_NUMBER_FACT);
+	for (i = 1; i <= MAX_NUMBER_FACT; i++)
+	{
 		factorial = 1;
-		for (j = 1; j <= i; j++) {
+		for (j = 1; j <= i; j++)
+		{
 			factorial = factorial * j;
 		}
-		printk("The factorial of %2d = %ld\n", i, factorial);
+		LOG_INF("The factorial of %2d = %ld", i, factorial);
 	}
-	printk("_______________________________________________________\n");
 	/*Important note!
-	Code in ISR runs at a high priority, therefore, it should be written with timing in mind.
-	Too lengthy or too complex tasks should not be performed by an ISR, they should be deferred
-	to a thread.
+	Code in ISR runs at a high priority, therefore,
+	it should be written with timing in mind.
+	Too lengthy or too complex tasks should not be performed by an ISR,
+	they should be deferred to a thread
 	*/
 }
 
@@ -50,20 +55,33 @@ int main(void)
 {
 	int ret;
 	/* STEP 6 - Write some logs */
-	printk("nRF Connect SDK Fundamentals - Lesson 4 - Exercise 1\n");
+	int exercise_num = 2;
+	uint8_t data[] = {'H', 'e', 'l', 'l', 'o', ' ',
+					  'H', 'U', 'N', 'G', 'N', 'N', '2'};
+	// Printf-like messages
+	LOG_INF("nRF Connect SDK Fundamentals");
+	LOG_INF("Lesson 4 Exercise %d", exercise_num);
+	LOG_DBG("A log message in debug level");
+	LOG_WRN("A log message in warning level!");
+	LOG_ERR("A log message in Error level!");
+	// Hexdump some data
+	LOG_HEXDUMP_INF(data, sizeof(data), "Sample Data!");
 
 	/* Only checking one since led.port and button.port point to the same device, &gpio0 */
-	if (!device_is_ready(led.port)) {
+	if (!device_is_ready(led.port))
+	{
 		return -1;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		return -1;
 	}
 
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		return -1;
 	}
 
@@ -72,7 +90,8 @@ int main(void)
 	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
 
 	gpio_add_callback(button.port, &button_cb_data);
-	while (1) {
+	while (1)
+	{
 		k_msleep(SLEEP_TIME_MS);
 	}
 }
